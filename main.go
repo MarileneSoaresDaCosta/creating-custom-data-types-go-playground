4cd23d1f2a3672bf1d2796bd3479cbf877546076
Marilene Soares da Costa <cmarilene@vmware.com>
2023-04-06 10:49:26 -0600

    update: embedded structs and interfaces

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 )
 
 func main() {
-	p := organization.NewPerson("Jon", "Snow")
+	p := organization.NewPerson("Jon", "Snow", organization.NewEuropeanUnionIdentifier("123-45-6789", "Germany"))
 	err := p.SetTwitterHandler("@king_in_the_north")
+	fmt.Printf("%T\n", organization.TwitterHandler("test"))
 	if err != nil {
 		fmt.Println("An error occurred setting the twitter handler: %s\n", err.Error())
 	}
 	println(p.TwitterHandler())
-
+	println(p.TwitterHandler().RedirectUrl())
 	println(p.ID())
+	println(p.Country())
 	println(p.FullName())
 
 }
--- organization/person.go
+++ organization/person.go
@@ -6,22 +6,84 @@ import (
 	"strings"
 )
 
+// this creates an alias; underneath, it is still a string
+//type TwitterHandler = string
+
+type Handler struct {
+	handle string
+	name   string
+}
+
+// alternatively, we can simply us a type declaration
+type TwitterHandler string
+
+// this new method - RedirectUrl -  would not be possible to define if we were using an alias
+func (th TwitterHandler) RedirectUrl() string {
+	cleanHandler := strings.TrimPrefix(string(th), "@")
+	return fmt.Sprintf("https://www.twitter.com/%s", cleanHandler)
+}
+
 type Identifiable interface {
 	// define here the functions I want
 	ID() string
 }
 
+type Citizen interface {
+	Identifiable
+	Country() string
+}
+type socialSecurityNumber string
+
+func NewSocialSecurityNumber(value string) Citizen {
+	return socialSecurityNumber(value)
+}
+
+func (ssn socialSecurityNumber) ID() string {
+	return string(ssn)
+}
+
+func (ssn socialSecurityNumber) Country() string {
+	return "United States of America"
+}
+
+type europeanUnionIdentifier struct {
+	id      string
+	country string
+}
+
+func NewEuropeanUnionIdentifier(id, country string) Citizen {
+	return europeanUnionIdentifier{
+		id:      id,
+		country: country,
+	}
+}
+
+func (eui europeanUnionIdentifier) ID() string {
+	return eui.id
+}
+
+func (eui europeanUnionIdentifier) Country() string {
+	return fmt.Sprintf("EU: %s", eui.country)
+}
+
+type Name struct {
+	firstName string
+	lastName  string
+}
 type Person struct {
-	firstName      string
-	lastName       string
-	twitterHandler string
+	Name
+	twitterHandler TwitterHandler
+	Citizen
 }
 
 // go has no constructor, so to keep control of new obj, we use the following idiom
-func NewPerson(firstName, lastName string) Person {
+func NewPerson(firstName, lastName string, citizen Citizen) Person {
 	return Person{
-		firstName: firstName,
-		lastName:  lastName,
+		Name: Name{
+			firstName: firstName,
+			lastName:  lastName,
+		},
+		Citizen: citizen,
 	}
 }
 
@@ -29,27 +91,32 @@ func (p *Person) FullName() string {
 	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
 }
 
+// this is no longer necessary when we embedded the interface Identifiable directly on Person
+//func (p *Person) ID() string {
+//	return "12345"
+//}
+
 func (p *Person) ID() string {
-	return "12345"
+	return fmt.Sprintf("Person's identifier: %s", p.Citizen.ID())
 }
 
 // when editing state with a custom type, we must use a pointer
 // we need to use a pointer-base receiver
 // because a function makes a copy of each argument value, if a func needs to
-// update a var, we must pass the addres of the variable using a pointer
+// update a var, we must pass the address of the variable using a pointer
 // this is also important when large data is being passed
-// *** by convention, then we make all methods use the pointer receiver, even the read only ones
+// *** by convention, then we make all methods use the pointer receiver, even the read-only ones
 
-func (p *Person) SetTwitterHandler(handler string) error {
-	if len(handler) == 0 {
+func (p *Person) SetTwitterHandler(handler TwitterHandler) error {
+	if len(handler) == 0 { // we want the user to be able to create an empty string handler
 		p.twitterHandler = handler
-	} else if !strings.HasPrefix(handler, "@") {
+	} else if !strings.HasPrefix(string(handler), "@") {
 		return errors.New("twitter handler must start with an @ symbol")
 	}
 	p.twitterHandler = handler
 	return nil
 }
 
-func (p *Person) TwitterHandler() string {
+func (p *Person) TwitterHandler() TwitterHandler {
 	return p.twitterHandler
 }
